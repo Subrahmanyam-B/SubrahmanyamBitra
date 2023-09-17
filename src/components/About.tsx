74ea6241ef5dc0e0604783113e24fddd9416efaa
Subrahmanyam <subrahmanyam9124@gmail.com>
2023-09-17 18:35:03 +0530

    Scroll parallax

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const About: React.FC = () => {
   return (
-    <section className="flex flex-col bg-white py-20 text-3xl md:text-4xl ">
-      <div className="container mx-auto px-11"></div>
+    <section className="flex flex-col bg-white min-h-screen py-20 text-3xl md:text-4xl ">
+      <div className="container mx-auto px-11"> Coming Soon</div>
       About
     </section>
   );
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useCallback, useContext } from "react";
 import Image from "next/image";
+import { useRef , useEffect , useState } from "react";
+import { ScrollContext } from "@/utils/scroll-observer";
 
 const Hero: React.FC = () => {
+
+  const refContainer = useRef<HTMLDivElement>(null);
+  const {scrollY} = useContext(ScrollContext);
+
+  const [imageLoaded , setImageLoaded] = useState(false);
+
+  const handleImageLoaded = useCallback(()=>
+  setImageLoaded(true)
+  ,[]);
+
+  let progress = 0
+
+  const {current : elContainer} = refContainer;
+
+  if(elContainer){
+    progress = Math.min(1 , scrollY/ elContainer.clientHeight)
+  }
+
   return (
-    <div className="bg-[#dad7cd] min-h-screen p-5 overflow-x-hidden">
+    <div
+      ref={refContainer}
+      className="bg-[#dad7cd] min-h-screen p-5 overflow-x-hidden sticky top-0 -z-10 "
+      style={{
+        transform: `translateY(-${progress * 20}vh)`,
+      }}
+    >
       <div className="flex justify-between px-5 pt-2 md:px-10 md:pt-5 items-center relative z-10">
         <button className="w-14 h-14 md:w-20 md:h-20 glass hover:bg-white rounded-full flex justify-center items-center shadow-lg cursor-pointer">
           <Image src="/assets/logo.svg" width={100} height={100} alt="logo" />
@@ -20,17 +46,24 @@ const Hero: React.FC = () => {
           alt="hero"
           width={1000}
           height={1000}
-          className="w-[50rem] 2xl:h-[55rem] 3xl:h-[60rem] -translate-x-8 md:w-auto mx-auto"
+          className={` 2xl:h-[55rem] 3xl:h-[60rem]  md:h-[40rem] -translate-x-8 w-[50rem] md:w-auto transition-all duration-1000 mx-auto ${
+            imageLoaded ? "opacity-100" : "opacity-0 -translate-y-10"
+          }`}
+          onLoad={handleImageLoaded}
         />
-        <div className="glass text-white absolute top-[13%] left-[25%] md:top-[27%] md:right-[55%] px-6 py-5 rounded-2xl flex-col items-start">
-          <div className="text-xs md:text-base pl-1">I&apos;m</div>
+        <div className="glass text-white absolute top-[13%] left-[25%] md:left-auto md:top-[27%] md:right-[55%] px-4 py-3 md:px-7 md:py-6 rounded-2xl flex-col items-start">
+          <div className="text-xs md:text-base pl-1">Hi! I&apos;m</div>
           <div className="text-2xl md:text-3xl font-semibold">Subrahmanyam</div>
         </div>
-        <div className="glass text-white absolute top-[55%] right-[22%] md:right-[18%] px-5 py-4 rounded-2xl flex-col items-start">
-          <div className="text-base md:text-2xl font-semibold">FullStack Developer</div>
+        <div className="glass text-white absolute top-[55%] right-[24%] md:right-[34%] px-4 py-3 md:px-7 md:py-6 rounded-2xl flex-col items-start">
+          <div className="text-base md:text-2xl font-semibold">
+            FullStack Developer
+          </div>
         </div>
-        <div className="glass text-white absolute top-[75%] left-[25%] md:top-[60%] md:right-[65%] px-5 py-4 rounded-2xl flex-col items-start">
-          <div className="text-base md:text-2xl font-semibold">UI/UX Designer</div>
+        <div className="glass text-white absolute top-[75%] left-[25%] md:left-auto md:top-[60%] md:right-[55%] px-4 py-3 md:px-7 md:py-6 rounded-2xl flex-col items-start">
+          <div className="text-base md:text-2xl font-semibold">
+            UI/UX Designer
+          </div>
         </div>
       </div>
     </div>
