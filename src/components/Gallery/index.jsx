a30187205f867db2506c3bd22b2c54fd9c15ea7b
Subrahmanyam Bitra <subrahmanyam9124@gmail.com>
2024-07-16 12:16:27 +0530

    Added Project Content

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -4,6 +4,7 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Image from "next/image";
 import Rounded from "../../common/RoundedButton";
+import Link from "next/link";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -12,34 +13,73 @@ const Gallery = () => {
     {
       title: "AlgoArena",
       type: "Personal Project",
-      description: "Content Pending",
+      description:
+        "AlgoArena is a fullstack online coding judge that allows programmers improve their problem solving and DSA skills.",
       image: "/images/algoarena.png",
       color: "#21242b",
-      stack: ["React", "Node"],
+      stack: [
+        "react.svg",
+        "reactquery.svg",
+        "vitejs.svg",
+        "typescript.svg",
+        "nodejs.svg",
+        "postgresql.svg",
+      ],
+      link: "https://algoarena.subrahmanyam.site/",
     },
     {
       title: "Comeata",
       type: "Intern Project",
-      description: "Content Pending",
+      description:
+        "Comeata is an innovative, comprehensive food management platform tailored to support and scale the endeavors of food entrepreneurs.",
+      contribution:
+        "Worked as Frontend Lead for the entire project managing other interns.",
       image: "/images/comeata.png",
       color: "#d6d7dc",
-      stack: ["React", "Node"],
+      stack: [
+        "nextjs.svg",
+        "reactquery.svg",
+        "typescript.svg",
+        "nodejs.svg",
+        "directus.svg",
+        "postgresql.svg",
+      ],
+      link: "https://trycomeata.com/",
     },
     {
       title: "SHUYI",
       type: "Intern Project",
-      description: "Content Pending",
+      description:
+        "SHUYI is a cutting-edge e-commerce application specifically designed for the purchase of construction materials, offering a streamlined shopping experience.",
+      contribution:
+        "Developed an Admin Panel using Next.js for the Customer Relation Management(CRM) and Order Management",
       image: "/images/shuyi.webp",
       color: "#d6d7dc",
-      stack: ["React", "Node"],
+      stack: [
+        "nextjs.svg",
+        "reactquery.svg",
+        "typescript.svg",
+        "nodejs.svg",
+        "postgresql.svg",
+      ],
+      link: "https://play.google.com/store/apps/details?id=com.weframe.shyui&pcampaignid=web_share",
     },
     {
       title: "Relynk",
       type: "Intern Project",
-      description: "Content Pending",
+      description:
+        "Relynk is an Oslo-based Proptech company that’s making waves in the real estate industry. Their mission is to drive innovation by simplifying access to real-time data from commercial buildings. ",
+      contribution:
+        "Assisted in the revamp of landing page UI and docs UI along with the integration of Directus CMS",
       image: "/images/relynk.png",
       color: "#d6d7dc",
-      stack: ["React", "Node"],
+      stack: [
+        "nextjs.svg",
+        "tailwindcss.svg",
+        "typescript.svg",
+        "directus.svg",
+      ],
+      link: "https://relynk.io/",
     },
   ];
 
@@ -108,9 +148,62 @@ const Gallery = () => {
           <div className={styles.desktopContent}>
             {projects.map((project, index) => (
               <div key={index} className={styles.desktopContentSection}>
-                <p></p>
-                <h1>{project.title}</h1>
-                <p>{project.description}</p>
+                <h1>
+                  <div
+                    style={{
+                      display: "flex",
+                      marginBottom: "1rem",
+                    }}
+                  >
+                    <div className={styles.button2}>
+                      <p>{project.type}</p>
+                    </div>
+                  </div>
+                  {project.title}
+                </h1>
+                <div style={{ fontSize: "1.5rem" }}>
+                  {project.description}
+                  {project.contribution && (
+                    <p style={{ fontSize: "1.25rem" }}>
+                      <span style={{ fontFamily: "monospace" }}>
+                        Contribution:
+                      </span>{" "}
+                      {project.contribution}
+                    </p>
+                  )}
+                </div>
+                <div style={{ fontSize: "1.25rem", fontFamily: "monospace" }}>
+                  Technologies Used:
+                  <p style={{ display: "flex", gap: "1rem" }}>
+                    {project.stack.map((item, index) => (
+                      <Image
+                        key={index}
+                        src={"/images/" + item}
+                        width={50}
+                        height={50}
+                      />
+                    ))}
+                  </p>
+                  <Link
+                    href={project.link}
+                    style={{
+                      display: "flex",
+                    }}
+                  >
+                    <Rounded>
+                      <p
+                        style={{
+                          textDecoration: "none",
+                          color: "white",
+                          fontSize: "1.25rem",
+                          lineHeight: "0",
+                        }}
+                      >
+                        Link
+                      </p>
+                    </Rounded>
+                  </Link>
+                </div>
               </div>
             ))}
           </div>
